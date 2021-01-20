d47ca7f5773dbb9465289853323f6017bbb06a38
hassanreus <hassanseffiani@gmail.com>
2021-01-20 02:44:18 +0100

    images

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Axios from 'axios'
-// we should use route instead of using BrowserRoute.
 import { Router }from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './start/theme'
@@ -10,7 +9,6 @@ import history from './history/history'
 
 
 Axios.defaults.baseURL = "http://localhost:3001/"
-// Axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
