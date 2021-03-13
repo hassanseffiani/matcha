390ddea2242e36b27a88a2c7b4343d2d86e437d2
hassanreus <hassanseffiani@gmail.com>
2021-03-13 22:07:46 +0100

    seeds complet

--- server/util/database.js
+++ server/util/database.js
@@ -1,32 +1,14 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2")
 
 ///////////////////////////// get ip docker container /////////////////////////////
-const ip = require("docker-ip");
-
-const ipAdress = ip(); // 192.168.99.100 a "default" one
-
+const ipAdress = require("docker-ip")() // 192.168.99.100 a "default" one
 ///////////////////////////////////////////////////////////////////////////////////////
-
-const pool1 = mysql.createPool({
-  host: `${ipAdress}`,
-  user: "root",
-  password: "tiger",
-});
-
-
-pool1.query('CREATE DATABASE IF NOT EXISTS Matcha")
 
 const pool = mysql.createPool({
   host: `${ipAdress}`,
   user: "root",
   database: "Matcha",
   password: "tiger",
-});
-
-
-const allQuery =
-  'use Matcha; CREATE TABLE IF NOT EXISTS users(`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT)'
-pool.query(allQuery)
-
+})
 
 module.exports = pool.promise()
